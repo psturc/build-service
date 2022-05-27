9426a775eb854a1d56d26f4b667921e4b63d390f
Pavel Sturc <psturc@redhat.com>
2022-05-27 17:38:44 +0200

    test: decrease coverage

--- controllers/component_build_controller_unit_test.go
+++ controllers/component_build_controller_unit_test.go
@@ -17,79 +17,10 @@ limitations under the License.
 package controllers
 
 import (
-	"context"
 	"testing"
 
 	corev1 "k8s.io/api/core/v1"
 )
-
-func TestGetGitProvider(t *testing.T) {
-	type args struct {
-		ctx    context.Context
-		gitURL string
-	}
-	tests := []struct {
-		name       string
-		args       args
-		wantErr    bool
-		wantString string
-	}{
-		{
-			name: "github",
-			args: args{
-				ctx:    context.Background(),
-				gitURL: "git@github.com:redhat-appstudio/application-service.git",
-			},
-			wantErr:    true, //unsupported
-			wantString: "",
-		},
-		{
-			name: "github https",
-			args: args{
-				ctx:    context.Background(),
-				gitURL: "https://github.com/redhat-appstudio/application-service.git",
-			},
-			wantErr:    false,
-			wantString: "https://github.com",
-		},
-		{
-			name: "bitbucket https",
-			args: args{
-				ctx:    context.Background(),
-				gitURL: "https://sbose78@bitbucket.org/sbose78/appstudio.git",
-			},
-			wantErr:    false,
-			wantString: "https://bitbucket.org",
-		},
-		{
-			name: "no scheme",
-			args: args{
-				ctx:    context.Background(),
-				gitURL: "github.com/redhat-appstudio/application-service.git",
-			},
-			wantErr:    true, //fully qualified URL is a must
-			wantString: "",
-		},
-		{
-			name: "invalid url",
-			args: args{
-				ctx:    context.Background(),
-				gitURL: "not-even-a-url",
-			},
-			wantErr:    true, //fully qualified URL is a must
-			wantString: "",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got, err := getGitProvider(tt.args.gitURL); (got != tt.wantString) ||
-				(tt.wantErr == true && err == nil) ||
-				(tt.wantErr == false && err != nil) {
-				t.Errorf("UpdateServiceAccountIfSecretNotLinked() Got Error: = %v, want %v ; Got String:  = %v , want %v", err, tt.wantErr, got, tt.wantString)
-			}
-		})
-	}
-}
 
 func TestUpdateServiceAccountIfSecretNotLinked(t *testing.T) {
 	type args struct {
